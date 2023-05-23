367158539b4cd50fd731e55b273fe5c12360b760
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-23 15:49:15 +0800

    Initial Commit

---
+++ DinamicList/DinamicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinamicListApp.swift
+//  DinamicList
+//
+//  Created by Claudio Tendean on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DinamicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
